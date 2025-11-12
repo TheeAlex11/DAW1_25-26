3c953c497ccf995859dc51867cb6ba38bf7e0c57
Ale_Windows <thealex11pro@gmail.com>
2025-11-12 02:12:36 +0100

    Casa_Windows

--- prog/ut03/ejerciciosDepuracion/ejerUno.java
+++ prog/ut03/ejerciciosDepuracion/ejerUno.java
@@ -5,6 +5,8 @@ public class ejerUno {
         System.out.println(esPrimo(17) + " Debe ser true");
         System.out.println(esPrimo(2) + " Debe ser true");
         System.out.println(esPrimo(8) + " Debe ser false");
+        System.out.println("------------------------------------------------------------------");
+        System.out.println(contarPrimos(1, 10));
 
     }
     
@@ -16,7 +18,21 @@ public class ejerUno {
                 break;
             }
         }
-        
+
         return primo;
+    }
+    
+    public static Integer contarPrimos(Integer numeroUno, Integer numeroDos) {
+            if (numeroUno > numeroDos) {
+                System.out.println("El primer numero no puede ser inferior al segundo!");
+            }
+
+            Integer contador = 0;
+            for (Integer i = numeroUno; i <= numeroDos; i++) {
+                if (esPrimo(i)) {
+                    contador++;
+                }
+            }
+            return contador;
     }
 }
