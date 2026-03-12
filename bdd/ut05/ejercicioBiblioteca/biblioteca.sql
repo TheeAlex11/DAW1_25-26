4bbdf2e09f52a515ce7218973e6b3f6df32412a8
PortatilDAW <thealex11pro@gmail.com>
2026-03-12 19:39:19 +0100

    BDD 12/03

--- bdd/ut05/ejercicioBiblioteca/biblioteca.sql
+++ bdd/ut05/ejercicioBiblioteca/biblioteca.sql
@@ -139,3 +139,112 @@ INSERT INTO prestamos (id_socio,id_libro,fecha_prestamo,fecha_devolucion) VALUES
 INSERT INTO prestamos (id_socio,id_libro,fecha_prestamo,fecha_devolucion) VALUES (7,107,SYSDATE,NULL);
 INSERT INTO prestamos (id_socio,id_libro,fecha_prestamo,fecha_devolucion) VALUES (8,108,SYSDATE,NULL);
 
+/*
+=================================================================================================
+  5. Consultas de datos.
+=================================================================================================
+*/
+
+-- Consulta 1 Mostrar el nombre del socio y la fecha del prestamo
+SELECT s.nombre, p.fecha_prestamo
+FROM socios s
+INNER JOIN prestamos p ON s.id_socio = p.id_socio;
+
+-- Consulta 2 Mostrar el identificador del prestamo y el nombre del socio
+SELECT p.id_prestamo, s.nombre
+FROM prestamos p
+INNER JOIN socios s ON p.id_socio = s.id_socio;
+
+-- Consulta 3 Mostrar los prestamos ordenados por nombre del socio
+SELECT p.id_prestamo, s.nombre
+FROM prestamos p
+INNER JOIN socios s ON p.id_socio = s.id_socio
+ORDER BY s.nombre;
+
+-- Consulta 4 Mostrar los socios que han realizado algun prestamo
+SELECT DISTINCT s.nombre
+FROM socios s
+INNER JOIN prestamos p ON s.id_socio = p.id_socio;
+
+-- Consulta 5 Mostrar todos los socios con sus prestamos hayan hecho prestamos o no
+SELECT s.nombre, p.id_prestamo
+FROM socios s
+LEFT JOIN prestamos p ON s.id_socio = p.id_socio;
+
+-- Consulta 6 Mostrar los prestamos activos con el nombre del socio
+SELECT s.nombre, p.fecha_prestamo
+FROM socios s
+INNER JOIN prestamos p ON s.id_socio = p.id_socio
+WHERE p.fecha_devolucion IS NULL;
+
+-- Consulta 7 Mostrar todos los prestamos y si existe el socio asociado
+SELECT p.id_prestamo, s.nombre
+FROM prestamos p
+LEFT JOIN socios s ON p.id_socio = s.id_socio;
+
+-- Consulta 8 Mostrar todos los socios y todos los prestamos
+SELECT s.nombre, p.id_prestamo
+FROM socios s
+FULL OUTER JOIN prestamos p ON s.id_socio = p.id_socio;
+
+-- Consulta 9 Mostrar los socios que han realizado algun prestamo sin asociacion
+SELECT s.nombre
+FROM socios s
+WHERE s.id_socio IN (SELECT id_socio FROM prestamos);
+
+-- Consulta 10 Mostrar los socios que no han realizado ningun prestamo
+SELECT s.nombre
+FROM socios s
+WHERE s.id_socio NOT IN (SELECT id_socio FROM prestamos);
+
+-- Consulta 11 Mostrar todos los prestamos con la informacion de todos los libros
+SELECT p.id_prestamo, l.titulo, l.autor
+FROM prestamos p
+RIGHT JOIN libros l ON p.id_libro = l.id_libro;
+
+-- Consulta 12 Mostrar todos los socios y todos los prestamos, aunque no coincidan
+SELECT s.nombre, p.id_prestamo
+FROM socios s
+FULL JOIN prestamos p ON s.id_socio = p.id_socio;
+
+-- Consulta 13 Mostrar todos los libros y todos los prestamos aunque no coincidan
+SELECT l.titulo, p.id_prestamo
+FROM libros l
+FULL JOIN prestamos p ON l.id_libro = p.id_libro;
+
+-- Consulta 14 Mostrar los libros que han sido prestados al menos una vez (Sin asociacion)
+SELECT l.titulo
+FROM libros l
+WHERE l.id_libro IN (SELECT id_libro FROM prestamos);
+
+-- Consulta 15 Mostrar los libros que nunca han sido prestados(Sin asociacion)
+SELECT l.titulo
+FROM libros l
+WHERE l.id_libro NOT IN (SELECT id_libro FROM prestamos);
+
+-- Consulta 16 Mostrar los socios que han pedido el libro con la id "101" (Sin asociacion)
+SELECT s.nombre
+FROM socios s
+WHERE s.id_socio IN (SELECT id_socio FROM prestamos WHERE id_libro = 101);
+
+-- Consulta 17 Mostrar los libros pedidos por el socio con la id "1" (Sin asociacion)
+SELECT id_libro
+FROM prestamos
+WHERE id_socio = 1;
+
+-- Consulta 18 mostrar los libros cuyo stock es mayor que el stock medio de todos los libros
+SELECT titulo
+FROM libros
+WHERE stock > (SELECT AVG(stock) FROM libros);
+
+-- Consulta 19 Mostrar el nombre del socio, el titulo del libro y la fecha del prestamo
+SELECT s.nombre, l.titulo, p.fecha_prestamo
+FROM socios s
+INNER JOIN prestamos p ON s.id_socio = p.id_socio
+INNER JOIN libros l ON p.id_libro = l.id_libro;
+
+-- Consulta 20 Mostrar todos los socios y los prestamos que han realizado, junto con el titulo del libro
+SELECT s.nombre, p.id_prestamo, l.titulo
+FROM prestamos p
+FULL JOIN socios s ON p.id_socio = s.id_socio
+INNER JOIN libros l ON p.id_libro = l.id_libro;
